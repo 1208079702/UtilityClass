3247283e8522a78338e50f2e716f6651b727060c
wujiaze <1051277749@qq.com>
2018-12-06 18:56:00 +0800

    2018/12/06 周四

---
+++ Unity工具/工具类/Unity_相机镜面反射/MirrorCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MirrorCamera : MonoBehaviour
+{
+
+    private Camera _uiCamera;
+    void Start()
+    {
+        _uiCamera = transform.Find("UICamera").GetComponent<Camera>();
+
+        MirrorCameraDisplay(_uiCamera);
+    }
+
+    public static void MirrorCameraDisplay(Camera camera)
+    {
+        Matrix4x4 matri = camera.projectionMatrix;
+        matri *= Matrix4x4.Scale(new Vector3(-1, 1, 1)); // 镜面反射 ，即X轴反向
+        camera.projectionMatrix = matri;
+    } 
+}
