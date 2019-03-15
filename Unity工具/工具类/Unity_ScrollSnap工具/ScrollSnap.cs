3619fa827920043f9f25036fffab0afa61e94f41
wujiaze <1051277749@qq.com>
2019-03-15 15:56:38 +0800

    2019/03/15 周五

---
+++ Unity工具/工具类/Unity_ScrollSnap工具/ScrollSnap.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+[RequireComponent(typeof(ScrollRect))]
+public class ScrollSnap : MonoBehaviour, IEndDragHandler, IBeginDragHandler
+{
+    private ScrollRect _scrollRect;
+    private bool _isEndDrag;
+    private float _targetHorPos;
+    private List<float> posList;
+    private void Awake()
+    {
+        _isEndDrag = false;
+        _scrollRect = GetComponent<ScrollRect>();
+        posList = new List<float>();
+    }
+
+    private void Update()
+    {
+        if (_isEndDrag)
+        {
+            _scrollRect.horizontalNormalizedPosition = Mathf.Lerp(_scrollRect.horizontalNormalizedPosition,
+                _targetHorPos, Time.deltaTime * 4);
+        }
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _isEndDrag = false;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _isEndDrag = true;
+        // 判断与哪一个点最靠近
+        int index = 0;
+        float offMin = Mathf.Abs(posList[index] - _scrollRect.horizontalNormalizedPosition);
+        for (int i = 1; i < posList.Count; i++)
+        {
+            float tempoff = Mathf.Abs(posList[i] - _scrollRect.horizontalNormalizedPosition);
+            if (tempoff < offMin)
+            {
+                offMin = tempoff;
+                index = i;
+            }
+        }
+        _targetHorPos = posList[index];
+    }
+
+    public void SetPos(List<float> list)
+    {
+        _isEndDrag = false;
+        posList = list;
+    }
+
+}
--- Unity工具/工具类/Unity_设置窗口/WindowSetting.cs
+++ Unity工具/工具类/Unity_设置窗口/WindowSetting.cs
@@ -1,67 +1,44 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class WindowSetting : MonoBehaviour {
-
-    /// <summary>  
-    /// 窗口宽度  
-    /// </summary>  
-    private int winWidth;
-    /// <summary>  
-    /// 窗口高度  
-    /// </summary>  
-    private int winHeight;
-    /// <summary>  
-    /// 窗口左上角x  
-    /// </summary>  
-    private int winPosX;
-    /// <summary>  
-    /// 窗口左上角y  
-    /// </summary>  
-    private int winPosY;
-
-    [DllImport("user32.dll")]
-    static extern IntPtr SetWindowLong(IntPtr hwnd, int _nIndex, int dwNewLong);
-    [DllImport("user32.dll")]
-    static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-    [DllImport("User32.dll", EntryPoint = "FindWindow")]
-    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-
-    private IntPtr hwindow;
-
-
+public class WindowSetting : MonoBehaviour
+{
     const uint SWP_SHOWWINDOW = 0x0040;
     const int GWL_STYLE = -16;
     const int WS_BORDER = 1;
     const int WS_POPUP = 0x800000;
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern IntPtr SetWindowLong(IntPtr hwnd, int _nIndex, int dwNewLong);
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+    [DllImport("User32.dll", EntryPoint = "FindWindow", CharSet = CharSet.Auto)]
+    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
+
+    private IntPtr _winPtr;
     void Start()
     {
         //获取窗口句柄
-        hwindow = FindWindow(null, Consts.windowsIntPtr);
+        _winPtr = FindWindow(null, ProjectConsts.WindowIntPtr);
         StartCoroutine(FullScreen());
     }
 
     IEnumerator FullScreen()
     {
-        //窗口切换带边框
-        //Screen.SetResolution(Consts.screen_width,Consts.screen_height,false);
         //鼠标显示状态设置
-        Cursor.visible = Consts.showcursor;
+        Cursor.visible = ProjectConsts.IsShowCursor;
         yield return new WaitForSeconds(1);
-
-        if (Consts.fullscreen == "0")
+        if (ProjectConsts.SceneModel == 0)
         {
-            Screen.SetResolution(Consts.screen_width, Consts.screen_height, true);
+            // 全屏
+            Screen.SetResolution(ProjectConsts.ScreenWidth, ProjectConsts.ScreenHeight, true);
         }
-        else if (Consts.fullscreen == "1")
+        else if (ProjectConsts.SceneModel == 1)
         {
-            Screen.SetResolution(Consts.screen_width, Consts.screen_height, false);
+            //窗口非全屏
+            Screen.SetResolution(ProjectConsts.ScreenWidth, ProjectConsts.ScreenHeight, false);
         }
         else
         {
@@ -69,25 +46,17 @@ public class WindowSetting : MonoBehaviour {
             {
                 try
                 {
-                    winWidth = Screen.width;
-                    winHeight = Screen.height;
-                    //显示器支持的所有分辨率  
-                    //int i = Screen.resolutions.Length;
-                    //int resWidth = Screen.resolutions[i - 1].width;
-                    //int resHeight = Screen.resolutions[i - 1].height;
-                    winPosX = 0;
-                    winPosY = 0;
-                    SetWindowLong(hwindow, GWL_STYLE, WS_POPUP);
-                    bool result = SetWindowPos(hwindow, -1, winPosX, winPosY, winWidth, winHeight, SWP_SHOWWINDOW);
+                    int winPosX = (int)(1f * ProjectConsts.ScreenWidth / ProjectConsts.ScreenHorCount * ProjectConsts.ScreenHorIndex);
+                    int winPosY = (int)(1f * ProjectConsts.ScreenHeight / ProjectConsts.ScreenVerCount * ProjectConsts.ScreenVerIndex);
+                    SetWindowLong(_winPtr, GWL_STYLE, WS_POPUP);
+                    SetWindowPos(_winPtr, -1, winPosX, winPosY, ProjectConsts.ScreenWidth, ProjectConsts.ScreenHeight, SWP_SHOWWINDOW);
                 }
-                catch (Exception e )
+                catch (Exception e)
                 {
-                    Debug.LogError("屏幕设置报错:" + e);
+                    Debug.LogError("屏幕设置报错:" + e.Message);
                 }
             }
         }
-        //yield return new WaitForSeconds(5);
-        //SceneManager.LoadScene(1);
     }
 
 
